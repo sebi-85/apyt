dd9c31d7655ce8c08f6eb1e9ffd5c4f5909e20f5
Sebastian Eich <Sebastian.Eich@mp.imw.uni-stuttgart.de>
2022-04-07 14:10:55 +0200

    Add TAPSim to raw conversion

--- apyt/conv.py
+++ apyt/conv.py
@@ -22,6 +22,7 @@ The following methods are provided:
 * :meth:`raw_concat`: Concatenate multiple raw files to a single one.
 * :meth:`raw_to_ascii`: Convert a raw measurement file to a human-readable ASCII
   file.
+* :meth:`tapsim_to_raw`: Convert TAPSim ASCII file to raw file.
 
 
 .. |TAPSim| raw:: html
@@ -41,7 +42,8 @@ The following methods are provided:
 __version__ = '0.1.0'
 __all__ = [
     "raw_concat",
-    "raw_to_ascii"
+    "raw_to_ascii",
+    "tapsim_to_raw"
 ]
 #
 #
@@ -51,6 +53,7 @@ __all__ = [
 import numpy as np
 #
 # import some special functions
+from datetime import datetime
 from struct import pack, unpack
 #
 #
@@ -135,3 +138,103 @@ def raw_to_ascii(raw_file, ascii_file):
         #
         # convert binary data and write to file
         [f.write(fmt % unpack(_bin_fmt, i)) for i in data]
+#
+#
+#
+#
+def tapsim_to_raw(tapsim_file, raw_file, id_range_list):
+    """Convert TAPSim ASCII file to raw file.
+
+    This function enables the conversion from a TAPSim ASCII file to a raw file
+    for further processing (e.g. reconstruction). A certain subset of columns is
+    imported from the TAPSim file, manipulated accordingly to match the raw file
+    format, and eventually written to a binary file. A constant base voltage
+    is used for all events, the time of flight is arranged such that it is
+    constant for one distinct species. The epoch is set to a constant time plus
+    1 event/s, the pulse number corresponds to the evaporation event.
+
+    The conversion is illustrated in the following table:
+
+    =========  =========  ====================  ================================
+    raw        data type  TAPSim                comment
+    =========  =========  ====================  ================================
+    U_base     float32    5000 V                constant
+    U_pulse    float32    0                     zero
+    U_ref      float32    0                     zero
+    x_det      float32    col. 7                conversion from m to mm
+    y_det      float32    col. 8                conversion from m to mm
+    tof        float32    constant per sepcies  constant for one species, \
+                                                separation 50 ns
+    epoch      int32      946681200             (2000-01-01 00:00:00) \
+                                                + 1 event/s
+    pulse_num  uint32     0, 1, 2, ...          corresponds to evaporation event
+    =========  =========  ====================  ================================
+
+    Parameters
+    ----------
+    tapsim_file: str
+        The name of the TAPSim file.
+    raw_file: str
+        The name of the raw file.
+    id_range_list: list
+        The list of id ranges used for mapping the atomic species, each of type
+        `tuple` of length 2, specifing the respective minimum and maximum id.
+    """
+    #
+    #
+    # load evaporation index, atomic id, and detector xy-position from TAPSim
+    # file
+    print("Reading TAPSim file \"{0:s}\" ...".format(tapsim_file))
+    data = np.loadtxt(tapsim_file, skiprows = 46, usecols = (0, 1, 7, 8))
+    #
+    #
+    # initialize empty array for mapped atomic ids
+    id = np.full(len(data), -1, dtype = int)
+    #
+    # loop through id ranges
+    for id_range in id_range_list:
+        # set index of current id range
+        i = id_range_list.index(id_range)
+        #
+        # map atomic id if in current range
+        id = np.where(
+            (id_range[0] <= data[:, 1]) & (data[:, 1] <= id_range[-1]), i, id)
+    #
+    # check whether all ids have been mapped
+    if np.count_nonzero(id == -1) > 0:
+        print("ERROR: Unspecified id detected. Please check your id ranges to "
+              "cover all occuring ids ({0:d}, {1:d}).".format(
+                  int(data[:, 1].min()), int(data[:, 1].max())))
+        exit(1)
+    #
+    #
+    # set arbitrary timestamp required in raw file
+    epoch = datetime(2000, 1, 1, 0, 0, 0).timestamp()
+    #
+    #
+    # set voltages
+    voltage = (5000.0, 0.0, 0.0)
+    #
+    #
+    # set data types for structured array
+    dt = np.dtype([
+        ('x_det', np.float32), ('y_det', np.float32), ('tof', np.float32),
+        ('epoch', np.int32),   ('pulse_num', np.uint32)])
+    #
+    # create and fill structured array
+    data_str = np.empty((len(data)), dtype = dt)
+    data_str['x_det']     = data[:, 2] * 1000  # m to mm
+    data_str['y_det']     = data[:, 3] * 1000  # m to mm
+    data_str['tof']       = id * 50.0 + 50.0   # tof grouped by atomic id
+    data_str['epoch']     = data[:, 0] + epoch # event id plus time offset
+    data_str['pulse_num'] = data[:, 0]         # event id
+    #
+    # convert structured array to list for faster iterator
+    data_l = data_str.tolist()
+    #
+    #
+    # open output file for writing
+    print("Writing binary file \"{0:s}\" ...".format(raw_file))
+    with open(raw_file, 'wb') as f:
+        # loop through events
+        [f.write(pack(_bin_fmt, *voltage, *i)) for i in data_l]
