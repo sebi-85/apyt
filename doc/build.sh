7dac717e8e481b38a03f89eac07186b455cf695d
Sebastian Eich <Sebastian.Eich@mp.imw.uni-stuttgart.de>
2024-10-17 01:03:30 +0200

    Update requirements file and build script

--- doc/build.sh
+++ doc/build.sh
@@ -1,5 +1,12 @@
 #/bin/bash
 #
+# activate virtual environment
+source ../venv/bin/activate
+pip3 install -r ../requirements.txt
+#
 rm -rf build/
 sphinx-apidoc -o source -e -T -E ../apyt
 sphinx-build source/ build/
+#
+# deactivate virtual environment
+deactivate
