0ed3c2dfe4847703951fd8debe555dd781593167
Sebastian Eich <Sebastian.Eich@mp.imw.uni-stuttgart.de>
2025-07-22 18:03:46 +0200

    Update build script for package documentation

--- doc/build.sh
+++ doc/build.sh
@@ -1,12 +1,47 @@
-#/bin/bash
+#/bin/bash -e
 #
-# activate virtual environment
-source ../venv/bin/activate
-pip3 install -r ../requirements.txt
+# Simple script for building the package documentation.
 #
+#
+#
+#
+# set script directory
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+#
+#
+# store current working directory
+PWD=$(pwd)
+#
+#
+#
+#
+# change into doc folder
+cd ${SCRIPT_DIR}/
+#
+#
+# create and activate virtual environment
+if [ ! -d "../venv" ]; then
+    echo -n "Creating virtual environment…"
+    python3 -m venv --prompt APyT ../venv
+    source ../venv/bin/activate
+    pip3 install -r ../requirements/requirements.txt > /dev/null
+    echo " done."
+else
+    source ../venv/bin/activate
+fi
+#
+#
+# build documentation
 rm -rf build/
 sphinx-apidoc -o source -e -T -E ../apyt
 sphinx-build source/ build/
 #
+#
 # deactivate virtual environment
 deactivate
+#
+#
+#
+#
+# change back into initial directory
+cd $PWD
