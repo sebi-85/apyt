f563091e0e3dd62f76a90be19ce67a843edeafac
Sebastian Eich <Sebastian.Eich@mp.imw.uni-stuttgart.de>
2025-08-19 15:53:26 +0200

    Rename requirements file for package building

--- tools/build_package.sh
+++ tools/build_package.sh
@@ -24,7 +24,7 @@ if [ ! -d "venv_build" ]; then
     echo -n "Creating virtual environment for package building…"
     python3 -m venv --prompt build venv_build
     source venv_build/bin/activate
-    pip3 install -r requirements/build_requirements.txt > /dev/null
+    pip3 install -r requirements/requirements_build.txt > /dev/null
     echo " done."
 else
     source venv_build/bin/activate
